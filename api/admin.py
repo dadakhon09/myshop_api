e0c05f4920d2613d3facbcc4cb9dd11c493ecf5e
dadakhon09 <odadaxon27@mail.ru>
2019-08-15 11:08:02 +0500

    wtfs

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from api.models import UserProfile, Partner, Action, Negotiation, Contract, Process, Tariff, MediaPlan, Settings, Diary, Day
+from api.models import UserProfile, Partner, Action, Negotiation, Contract, Process, Tariff, MediaPlan, Settings, Diary, \
+    Day, Payment
 
 # admin.site.register(Answer)
 admin.site.register(UserProfile)
 admin.site.register(Partner)
+admin.site.register(Payment)
 admin.site.register(Action)
 admin.site.register(Negotiation)
 admin.site.register(Contract)
--- api/pagination.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.pagination import (
-    PageNumberPagination,
-    LimitOffsetPagination,
-)
-
-class UserPageNumberPagination(PageNumberPagination):
-    page_size = 1
-    max_page_size = 10
-
